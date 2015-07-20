db038fb319de123fadffc280c745f70502631b82
Simon Morley <simon@polkaspots.com>
2015-07-20 15:55:02 +0100

    Deploy to secure-mesa test

--- Gruntfile.js
+++ Gruntfile.js
@@ -50,7 +50,8 @@ module.exports = function (grunt) {
         constants: {
           API_END_POINT: 'https://c7e5c5a6.ngrok.io/api/v1',
           // API_END_POINT: 'https://api.polkaspots.com/api/v1',
-          API_URL: 'https://api.polkaspots.com'
+          // API_URL: 'https://api.polkaspots.com'
+          API_URL: 'https://c7e5c5a6.ngrok.io'
         }
       }
     },
