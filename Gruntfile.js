04256c8f51be1410e58334024d43a785df217b09
Simon Morley <simon@polkaspots.com>
2015-08-03 15:59:04 +0100

    beta server

--- Gruntfile.js
+++ Gruntfile.js
@@ -49,8 +49,8 @@ module.exports = function (grunt) {
         },
         constants: {
           // API_END_POINT: 'https://c7e5c5a6.ngrok.io/api/v1',
-          API_END_POINT: 'https://api.polkaspots.com/api/v1',
-          API_URL: 'https://api.polkaspots.com'
+          API_END_POINT: 'https://beta.polkaspots.com/api/v1',
+          API_URL: 'https://beta.polkaspots.com'
           // API_URL: 'https://c7e5c5a6.ngrok.io'
         }
       }
