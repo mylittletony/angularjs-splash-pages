e39f7d457cd093af3f9121372e5e7c3467fd0469
Ewan <ewan.s@cucumberwifi.io>
2017-01-16 14:14:59 +0000

    setup

--- client/scripts/config.js
+++ client/scripts/config.js
@@ -2,9 +2,9 @@
 
  angular.module('config', [])
 
-.constant('API_END_POINT', 'http://mywifi.dev:8080/api/v1')
+.constant('API_END_POINT', 'http://api.ctapp.dev:8080/api/v1')
 
-.constant('API_URL', 'http://mywifi.dev:8080')
+.constant('API_URL', 'http://api.ctapp.dev:8080')
 
 .constant('ENVIRONMENT', 'development')
 
